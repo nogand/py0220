0f8ce857f15703d36d76d6f1afa2155dcdc9f893
Alf Delgado <github@delhelsa.com>
2020-04-22 20:49:34 -0600

    Segunda versión con función recursiva que trae una lista de la serie hasta el elemento n

--- Fibonacci/fibonacci.py
+++ Fibonacci/fibonacci.py
@@ -6,7 +6,7 @@
 """
 
 def fibo(n):
-    # Recibe un número entero mayor a cero n, y devuelve el elemento n-ésimo de la serie de Fibonacci
+    # Recibe un número n, entero mayor a cero, y devuelve el elemento n-ésimo de la serie de Fibonacci
     if n <= 2:
         return 1
     else :
@@ -18,4 +18,18 @@ def fibo(n):
    fibo(3) -> fibo(2)+fibo(1) -> 1+1 -> 2
    fibo(4) -> fibo(3)+fibo(2) -> (fibo(2)+fibo(1))+1 -> (1+1)+1 -> 3
    fibo(5) -> fibo(4)+fibo(3) -> (fibo(3)+fibo(2))+(fibo(2)+fibo(1)) -> ((fibo(2)+fibo(1))+1)... -> 5
+"""
+
+def fiblist(n):
+    # Recibe un número n, entero mayor a cero, y devuelve una lista de los elementos de la serie de Fibonacci hasta ese número.
+    if n == 1 :
+        return [ 1 ]
+    else :
+        return fiblist(n-1)+[ fibo(n) ]
+
+"""
+   fiblist(1) -> [ 1 ]
+   fiblist(2) -> fiblist(1)+[ fibo(2) ] -> [ 1 ] + [ 1 ] -> [1, 1]
+   fiblist(3) -> fiblist(2)+[ fibo(3) ] -> [1, 1] + [ 2 ] -> [1, 1, 2] 
+   fiblist(4) -> fiblist(3)+[ fibo(4) ] -> [1, 1, 2] + [ 3 ] -> [1, 1, 2, 3]
 """
--- Fibonacci/main.py
+++ Fibonacci/main.py
@@ -2,4 +2,4 @@
 from fibonacci import *
 
 n=int(input("Por favor indique hasta qué número generar: "))
-print(fibo(n))
+print(fiblist(n))
